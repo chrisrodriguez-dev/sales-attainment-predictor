1ad1882e7e5ff00bb12021bf7b88e4d2a2fe3d0f
Chris 💪🏽 <chris@Mac.fios-router.home>
2025-05-24 11:10:12 -0400

    Library of data preprocessing, data loading, and model training/buiding functions

--- model.py
+++ model.py
@@ -6,5 +6,23 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from utils import load_transactions
+
+
+def clean_data(df):
+    df['Sales Attain'] = df['Sales Attain'].str.rstrip('%').astype(float)
+    df['Trade-In Opp Att Rt'] = df['Trade-In Opp Att Rt'].str.rstrip('%').astype(float)
+    df['Overall CSAT'] = df['Overall CSAT'].astype(float)
+
+#imputing/eliminating and encoding features
+def preprocess_data(df):
+    pass
+
+def create_lag_features(df):
+    pass
+
+def train_model(df):
+    pass
+
 
 
