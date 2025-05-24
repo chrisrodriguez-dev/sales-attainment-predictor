e4c50016e7d8d9424054889731a487ebc8d16375
Chris 💪🏽 <chris@Mac.fios-router.home>
2025-05-24 11:08:41 -0400

    Refactor main.py to use load_transactions from utils.py

--- main.py
+++ main.py
@@ -5,45 +5,21 @@ import numpy as np
 import os
 import plotly.express as px
 import json
+from utils import load_transactions 
 
-st.set_page_config(page_title="Sales Tracker/Predictor", page_icon = "💵")
-
-st.write("This will be an app where I track/predict future sales at AT&T")
-def load_transactions(file):
-    try:
-        df = pd.read_csv(file, header = 4)
-        df.columns = [col.strip() for col in df.columns]
-        df = df.drop_duplicates()
-        df = df.iloc[3:]
-        df = df[df['Sales Attain'] != '0.00%']
-        df = df.loc[:, ~(df == '-').all()]
-        df = df.reset_index(drop=True)
-   
-
-        df_compact = df[df['Time Period'] == 'MTD'].copy()
-
-        
-
-        st.write(df_compact)
-       
-        return df
-    except Exception as e:
-        st.error(f"Error Processing File: {file}")
-        return None
+st.set_page_config(page_title="Sales Tracker/Predictor", page_icon = "📊")
 
 def main():
-    uploaded_file = st.file_uploader("Upload your csv", type = ["csv"])
-
-    if uploaded_file is not None:
-        df = load_transactions(uploaded_file)
-        return df
-    return None
-
-
-## Only run the code below if this file is being run directly    
+    uploaded_file = st.file_uploader("Upload your csv", type=["csv"])
+    try:
+        if uploaded_file is not None:
+            df = load_transactions(uploaded_file)
+            st.write(df)  # View the cleaned df
+            return df
+    except Exception as e:
+        st.write(f"Error loading in file: {uploaded_file}")
+        return None
+    
+# Only run the main function if this file is being run directly
 if __name__ == "__main__":
     main()
-
-
-
-
